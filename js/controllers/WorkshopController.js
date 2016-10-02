4faad9371aadfdf3ed7423dc685bd8b7d0863d7f
Zack <Zack Hardy>
2016-10-01 22:34:06 -0500

    WorkshopController and so on

---
+++ js/controllers/WorkshopController.js
@@ -0,0 +1,47 @@
+app.controller('WorkshopController', ['$scope', function ($scope){
+	$scope.workshops = [
+						{
+							writer:"Paul Lisicky",
+							type: "Creative Nonfiction",
+							description: "The ideal writing workshop is a place where a variety of forms are encouraged and respected, where we attempt to create a version of a model literary community: a thriving ecosystem, as Richard Powers might call it, rather than a monoculture. This is a class for those looking to revive a memoir or creative nonfiction project. We&apos;ll spend the first three days of the week reading and thinking about the work of other writers (Hilton Als, Nick Flynn, and Heidi Julavits, among others), generating material inspired by those selections. We&apos;ll spend the final two days talking about your new work. Along the way we&apos;ll work hard, have fun, and make sure delight isn&apos;t an enemy to seriousness. "
+						},
+						{
+							writer:"Stephen Graham Jones",
+							type: "Fiction",
+							description: "In here we'll worry the nuts and bolts of fiction&ndash;the mechanical aspects&ndash;but we'll also try to engage the magic and the sweep of fiction. The goal is always to pay the reader back for the time they're investing in your words. Make them laugh, let them cry, take away their sleep or tell them they're not as alone as they might think. And don't shy away from the fantastic, from the genres. The only things you should shy away from are muddied narrative, turgid prose, and ever being timid on the page. Dare to make a fool of yourself. Dare to be naked in the story. Write what you don't think you can write. Otherwise, you'll never get anywhere interesting."
+						},
+						{
+							writer:"Sam Michel",
+							type: "Fiction",
+							description: "This workshop will focus primarily on craft.  We will look closely at sentence composition and how the sounds and cadences of sentences work most powerfully to deliver the sense of character, place, and sentiment we intend to deliver.  We will talk some, for instance, about what Robert Frost meant by the &quot;cave of the mouth,&quot; and Twain's assertion that the difference between the right word and the wrong word is like the&quot;difference between lightning and a lightning bug.&quot;  We will consider the generative properties of language, and how a strict attention to the language we deploy conveys us to places and thoughts and feeling we might never have reached on our own.  What is it to be inspired?  How is our page suffused with the presence of a life?  Emerson said he was always delighted in reading when he came to the page expecting an author, and found instead a person.  We will attempt in this workshop, then, to find the means to present ourselves on the page as the people we most simply and powerfully are."
+						},
+						{
+							writer:"Joy Williams",
+							type: "Fiction",
+							description: "Participants&apos; new, raw material will provide the basis for this workshop. We will be working on three to four stories per session; the last session will be devoted to individual manuscript consultation (number of participants permitting). In addition to working with participant&apos;s manuscripts, we&apos;ll also be discussing some (more or less, classic or strange) stories to explore the techniques to make them effective."
+						},
+						{
+							writer:"Timothy Donnelly",
+							type:"Poetry",
+							description:"Often when writing a poem we perform the mystery of drawing from the reservoir of what we&apos;ve already experienced, thought, or felt or, better, from the stream of what we come to imagine, think, or feel in the throes of writing, in order to give it shape, linguistic form &mdash; the &quot;local habitation and a namee&quot; that Theseus says the poet&apos;s pen pens &quot;airy nothing&quot; in in <em>A Midsummer Night&apos;s Dream</em>. We set, in language, a contour to a consciousness that might start as ours but then becomes its own. In this workshop we will experiment with imparting a sense of fixity and focus to the flow of what we think and feel&ndash;the confluence of the ineffable with the banal and every emergent water in between&ndash;through the use of different kinds and varying degrees of patterning and formal regularity. While this won&apos;t be a workshop in traditional forms and meters, we will acknowledge how traditional poetic practice has, at its best, served to gratify the mind&apos;s seemingly opposite appetites for stability and surprise, sameness and variety, constancy and change. We will explore new ways of achieving the sense of &quot;unity in multeity&quot; that Coleridge, like so many before and after him, has identified as &quot;the principle of beauty.&quot; Most importantly, we will consider our work as a site where chaos meets containment, and how the struggle to control and to rebel against control can become not merely a compelling formal property of our writing but also a crucial aspect of its significance."
+						},
+						{
+							writer:"Dorothea Lasky",
+							type:"Poetry",
+							description: "In our workshop, we will explore what a poem is, what it can do, and what it will do, especially to you, and what it can and will do to your readers. We will examine your own poems closely in order to consider larger issues of craft and poetics. You will be asked constantly to re-imagine what your poems can become and it is the hope of the workshop that you will see your work transformed by the end of our time together. Writing exercises will be shared with you in order to aid this transformation and to push your practice, particularly those which ask you to engage with the everyday and the beauty of objects, things, and people. You will also be urged to share with the group: ideas, poems, songs, paintings, cities, crystals, animals, colors, theories, small jars filled with lead, heroes and heroines, gods and goddesses, disciplines, famous people and family members, foods, and flowers that inspire you and to create your own poetry exercises that help us all to see poetry anew. In doing all of this work, we will begin to create poems that are &quot;Music to sleep in,&quot; as Jack Spicer might have or might still urge us to do."
+						},
+						{
+							writer:"Harryette Mullen",
+							type:"Poetry",
+							description: "This workshop will explore possible uses for interrogation, affirmation, and negation in the practice of poetry. What happens when a poem asks questions, or when it affirms, confirms, negates, or denies what a reader might assume or believe?"
+						},
+						{
+							writer:"Dara Wier",
+							type:"Poetry",
+							description:"In our workshop you'll be invited to bring us your most questionable poems, your almost there poems, your inklings of poems, your most difficult (especially to you) poems, your poems in progress, your poems done and finished, complete and mysterious.  (whether this be in poem or in prose) And with them your concerns, risk-taking questions, gut feelings, risks, and instincts, your experience as a writer and as a reader, and your understanding of why poems matter, to you, and anyone. You'll be invited to bring us poems by poets you admire so we can gain a collective reading experience we can talk about, refer to, and enjoy. You'll be given a chance if you chose it,  to tell us how you happen to come to be writing poetry and what you hope your poetry will be up to.  You'll be invited to ask questions, tell us things, direct our conversation toward subjects you feel significant to poets gathering over poetry, in order to love it, to question it and to further its life in the future. Your work is the heart and soul and brain of our meetings and it will be our main focus."
+						}
+
+	]
+
+
+}])
